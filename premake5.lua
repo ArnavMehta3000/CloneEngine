4bb18dda355ffc36f5080ee54c57ec132c5235e7
Arnav Mehta <arnavdatt@gmail.com>
2023-11-09 22:32:32 +0000

    Removed auto generated files

--- premake5.lua
+++
@@ -1,9 +0,0 @@
-workspace "TestGame"
-    configurations { "Debug", "Release" }
-    architecture "x64"
-    location "E:/Dev/CloneEngine"
-    startproject "Runtime"
-
-include "E:/Dev/CloneEngine/Engine/BuildEngine.lua"
-include "E:/Dev/CloneEngine/Runtime/BuildRuntime.lua"
-include "E:/Dev/CloneEngine/TestGame/BuildTestGame.lua"
