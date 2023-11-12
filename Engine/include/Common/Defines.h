98f5e9049a7929cf4c28d8d65307857bf8d4df04
Arnav Mehta <arnavdatt@gmail.com>
2023-11-12 02:41:05 +0000

    Added fps-delta time conversion macros

--- Engine/include/Common/Defines.h
+++ Engine/include/Common/Defines.h
@@ -2,6 +2,12 @@
 
 namespace Clone::Common
 {
+// Convert value to string
 #define STRINGIFY(x) #x
+// Convert value to wide string
 #define STRINGIFYW(value) L ## #value
+// Convert frame rate to delta time
+#define FPS_TO_DT(fps) 1.0 / fps
+// Convert delta time to frame rate
+#define DT_TO_FPS(dt) 1.0 / dt
 }
