5537a2635177e08ca34472c3a11a1a5193e1ab50
Arnav Mehta <arnavdatt@gmail.com>
2023-11-22 00:12:15 +0000

    Added property macros

--- Engine/Common/Property.h
+++ Engine/Common/Property.h
@@ -3,16 +3,16 @@
 namespace Clone::Common
 {
 // Creates empty getter-setter property macro
-#define CLONE_PROPERTY(type,  propName)\
+#define CLONE_PROPERTY(type, propName)\
 public:\
 	__declspec(property(get = Get##propName, put = Set##propName)) type propName;\
-	type Get##propName();\
+	type Get##propName() const;\
 	void Set##propName(type value);\
 private:\
 	type m_##propName;
 
 // Creates const getter-setter property macro
-#define CLONE_CONST_PROPERTY(type,  propName)\
+#define CLONE_CONST_PROPERTY(type, propName)\
 public:\
 	__declspec(property(get = Get##propName, put = Set##propName)) type propName;\
 	const type& Get##propName() const;\
@@ -21,20 +21,20 @@ private:\
 	type m_##propName;
 
 // Auto creates getter-setter property macro
-#define CLONE_AUTO_PROPERTY(type,  propName)\
+#define CLONE_AUTO_PROPERTY(type, propName)\
 public:\
 	__declspec(property(get = Get##propName, put = Set##propName)) type propName;\
-	type Get##propName() { return m_##propName; }\
+	type Get##propName() const { return m_##propName; }\
 	void Set##propName(type value) { m_##propName = value; }\
 private:\
 	type m_##propName;
 
 // Auto creates getter-setter property macro
-#define CLONE_CONST_AUTO_PROPERTY(type,  propName)\
+#define CLONE_CONST_AUTO_PROPERTY(type, propName)\
 public:\
 	__declspec(property(get = Get##propName, put = Set##propName)) type propName;\
 	const type& Get##propName() const { return m_##propName; }\
-	void Set##propName(type value) const { m_##propName = value; }\
+	void Set##propName(type value) { m_##propName = value; }\
 private:\
 	type m_##propName;
 }
